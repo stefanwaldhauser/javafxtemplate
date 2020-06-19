d7dfffce95052a9a3a4a0b491b1f7dd27cd07667
Stefan Waldhauser <stefan.waldhauser@tum.de>
2020-06-19 18:07:31 +0200

    Added JavaFX Template

---
+++ src/com/example/main/Example.java
@@ -0,0 +1,8 @@
+package com.example.main;
+
+public class Example {
+    public static void main(String[] args) {
+        //This is a workaround for a known issue when starting JavaFX applications
+        ExampleApplication.startApp(args);
+    }
+}
---
+++ src/com/example/main/ExampleApplication.java
@@ -0,0 +1,32 @@
+package com.example.main;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * This class is the root of the JavaFX Application.
+ */
+public class ExampleApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The whole game will be executed through the launch() method
+     */
+    public static void startApp(String[] args) {
+        launch(args);
+    }
+
+}
